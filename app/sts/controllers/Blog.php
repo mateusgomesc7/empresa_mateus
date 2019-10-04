bf3c1f9bb611b1b6eb4f812607db7f9f0a7a7269
Mateus Gomes <mateusgomesc7@gmail.com>
2019-10-04 11:21:21 -0300

    Paginação no blog

--- app/sts/controllers/Blog.php
+++ app/sts/controllers/Blog.php
@@ -10,10 +10,15 @@ if (!defined('URL')) {
 class Blog {
 
     private $Dados;
+    private $PageId;
 
     public function index(){
-        $listar_art = new \Sts\models\StsBlog;
-        $this->Dados['artigos'] = $listar_art->listarArtigos();
+        $this->PageId = filter_input(INPUT_GET, 'pg', FILTER_SANITIZE_NUMBER_INT);
+        $this->PageId = $this->PageId ? $this->PageId : 1; // Manter na página 1, quando for nulo
+        // echo "<br><br><br> {$this->PageId}";
+        $listar_art = new \Sts\models\StsBlog();
+        $this->Dados['artigos'] = $listar_art->listarArtigos($this->PageId);
+        $this->Dados['paginacao'] = $listar_art->getResultadoPg();
 
         $carregarView = new \Core\ConfigView('sts/views/blog/blog', $this->Dados);
         $carregarView->renderizar();
